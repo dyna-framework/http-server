d2ede212538b691d8511afab52576a9e884720c9
FreeIntelligence <free04381@gmail.com>
2022-02-28 06:24:48 -0300

    Ahora el servidor obtiene el puerto y host desde el archivo de environment.

--- src/create-http-server.initializer.ts
+++ src/create-http-server.initializer.ts
@@ -1,16 +1,23 @@
 import { BaseInitializer } from '@dyna/core';
 import { createServer, Server } from 'http';
+import { env } from '@dyna/env';
 
+/**
+ * Extend application extra values
+ */
 declare module "@dyna/core" {
   export interface ExtraApplication {
     httpServer?: Server;
   }
 }
 
+/**
+ * Create HTTP Server
+ */
 export class CreateHttpServer extends BaseInitializer {
 
-  port: number = 8044;
-  host: string = 'localhost';
+  port: number = env('HTTP_SERVER_PORT', '8900');
+  host: string = env('HTTP_SERVER_HOST', 'localhost');
   server: Server = createServer(() => null);
 
   async register() {
@@ -19,8 +26,17 @@ export class CreateHttpServer extends BaseInitializer {
     }
   }
 
-  async boot() {
-    this.server.listen(this.port, this.host, () => console.log('Server is running on localhost'));
+  async boot(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.server.listen(this.port, this.host, () => {
+        this.onListening();
+        resolve();
+      });
+    });
+  }
+
+  onListening() {
+    console.log(`Server is running on ${this.host}:${this.port}`);
   }
 
 }
